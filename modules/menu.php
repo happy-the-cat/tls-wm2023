88378a81a3d3eda311db5a1fcb28a8fbe02c244b
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-15 19:34:13 +0800

    Create carousel via Bootstrap

---
+++ modules/menu.php
@@ -0,0 +1,28 @@
+<div id="menu">
+    <div id="menu-bg"></div>
+    <!-- CAROUSEL HERE -->
+    <div id="menu-carousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/icons/1-MariaRessa.svg" class="d-block w-100" alt="Maria Ressa">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/icons/5-HidilynDiaz.svg" class="d-block w-100" alt="Hidilyn Diaz">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/icons/6-SerenaWilliams.svg" class="d-block w-100" alt="Serena Williams">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/icons/7-DollyDeLeon.svg" class="d-block w-100" alt="Dolly de Leon">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#menu-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#menu-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
