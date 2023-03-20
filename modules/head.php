1ffef845cb533266058b2acfab3470ce002284dc
happy-the-cat <joyceyh.cai@gmail.com>
2023-03-21 06:48:57 +0800

    added font styles

--- modules/head.php
+++ modules/head.php
@@ -5,6 +5,8 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- Fontstyles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cardo|Inter|Lora">
     
     <!--TO DO-->
     <link rel="stylesheet" href="/css/styles.css">
