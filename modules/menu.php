890c02a38e8f989ba45f8806d2e614962fc226f6
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-23 19:37:22 +0800

    Added classes and ids

--- modules/menu.php
+++ modules/menu.php
@@ -3,14 +3,14 @@
     <div id="menu-slider">
         <button class="nav-btn" id="nav-prev"><</button>
 
-        <div class="icon"><a href=""><img class="menu-icon" src="./assets/icons/1-MariaRessa.png"></img></a></div>
-        <div class="icon"><a href=""><img class="menu-icon" src="./assets/icons/2-ChonaAbeledo.png"></img></a></div>
-        <div class="icon"><a href=""><img class="menu-icon" src="./assets/icons/3-ReinaReyes.png"></img></a></div>
-        <div class="icon"><a href=""><img class="menu-icon" src="./assets/icons/4-KarminaConstantino.png"></img></a></div>
-        <div class="icon"><a href=""><img class="menu-icon" src="./assets/icons/5-HidilynDiaz.png"></img></a></div>
-        <div class="icon"><a href=""><img class="menu-icon" src="./assets/icons/6-SerenaWilliams.png"></img></a></div>
-        <div class="icon"><a href=""><img class="menu-icon" src="./assets/icons/7-DollyDeLeon.png"></img></a></div>
-        <div class="icon"><a href=""><img class="menu-icon" src="./assets/icons/8-EdithTiempo.png"></img></a></div>
+        <div class="icon"><a href=""><img id="img-ressa" class="menu-icon" src="./assets/icons/1-MariaRessa.png"></img></a></div>
+        <div class="icon"><a href=""><img id="img-abeledo" class="menu-icon down" src="./assets/icons/2-ChonaAbeledo.png"></img></a></div>
+        <div class="icon"><a href=""><img id="img-reyes" class="menu-icon" src="./assets/icons/3-ReinaReyes.png"></img></a></div>
+        <div class="icon"><a href=""><img id="img-constantino" class="menu-icon down" src="./assets/icons/4-KarminaConstantino.png"></img></a></div>
+        <div class="icon"><a href=""><img id="img-diaz" class="menu-icon up" src="./assets/icons/5-HidilynDiaz.png"></img></a></div>
+        <div class="icon"><a href=""><img id="img-williams" class="menu-icon down" src="./assets/icons/6-SerenaWilliams.png"></img></a></div>
+        <div class="icon"><a href=""><img id="img-deleon" class="menu-icon" src="./assets/icons/7-DollyDeLeon.png"></img></a></div>
+        <div class="icon"><a href=""><img id="img-tiempo" class="menu-icon up" src="./assets/icons/8-EdithTiempo.png"></img></a></div>
         
         <button class="nav-btn" id="nav-next">></button>
     </div>
