6798ec70dd4aa85a3322831a32025cede8514e4c
happy-the-cat <joyceyh.cai@gmail.com>
2023-03-27 03:20:33 +0800

    removed test text

--- index.php
+++ index.php
@@ -6,14 +6,7 @@
     <?php require_once 'modules/menu.php'; ?>
     <?php require_once 'modules/vox-populi.php'; ?>
 
-    <!-- For testing purposes. Remove this later. -->
-    <div>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab doloremque mollitia deserunt incidunt totam cupiditate corrupti perspiciatis debitis. Tempora ratione atque enim quo quasi? Aperiam assumenda cum culpa esse illo.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt porro ducimus eveniet expedita fugiat sit, reiciendis magnam, atque quo placeat facilis ab aut quae, dolore deleniti? Ipsum sequi numquam optio.</p>
-    </div>
-
 	<!--TO DO: ARTICLES-->
-
 
 	<?php require_once 'modules/footer.php'; ?>
     
@@ -23,6 +16,6 @@
     <!-- Custom JS -->
     <script src="./js/carousel.js"></script>
     <script src="js/vox-populi.js"></script>
-	
+
 </body>
 </html>
