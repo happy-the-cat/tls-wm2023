8e8ac5bea5aa67ab1ed7a31c897184755af0ef9c
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-28 03:27:18 +0800

    Use bootstrap icons for menu.php

--- modules/menu.php
+++ modules/menu.php
@@ -1,7 +1,7 @@
 <div id="menu">
     <div id="menu-bg"></div>
     <div id="menu-slider">
-        <button class="nav-btn" id="nav-prev"><</button>
+        <button class="nav-btn" id="nav-prev"><i class="bi bi-chevron-left"></i></button>
 
         <div class="icon"><a href="women.php?w=maria-ressa"><img id="img-ressa" class="menu-icon" src="./assets/icons/1-MariaRessa.png" alt="Maria Ressa"></img></a></div>
         <div class="icon"><a href="women.php?w=chona-abeledo"><img id="img-abeledo" class="menu-icon down" src="./assets/icons/2-ChonaAbeledo.png" alt="Chona Abeledo"></img></a></div>
@@ -12,6 +12,6 @@
         <div class="icon"><a href="women.php?w=dolly-deleon"><img id="img-deleon" class="menu-icon" src="./assets/icons/7-DollyDeLeon.png" alt="Dolly de Leon"></img></a></div>
         <div class="icon"><a href="women.php?w=edith-tiempo"><img id="img-tiempo" class="menu-icon up" src="./assets/icons/8-EdithTiempo.png" alt="Edith Tiempo"></img></a></div>
         
-        <button class="nav-btn" id="nav-next">></button>
+        <button class="nav-btn" id="nav-next"><i class="bi bi-chevron-right"></i></button>
     </div>
 </div>
