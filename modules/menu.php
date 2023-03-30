d2123b6b1771fd70da5c6c992f4ca557b7a2ecc1
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-31 00:04:18 +0800

    Icon title on hover

--- modules/menu.php
+++ modules/menu.php
@@ -4,17 +4,16 @@
         <button class="nav-btn" id="nav-prev"><i class="bi bi-chevron-left"></i></button>
 
         <div id="menu-icons">
-        <div class="icon"><a href="women.php?w=maria-ressa"><img id="img-ressa" class="menu-icon" src="./assets/icons/1-MariaRessa.png" alt="Maria Ressa"></img></a></div>
-        <div class="icon"><a href="women.php?w=chona-abeledo"><img id="img-abeledo" class="menu-icon down" src="./assets/icons/2-ChonaAbeledo.png" alt="Chona Abeledo"></img></a></div>
-        <div class="icon"><a href="women.php?w=reina-reyes"><img id="img-reyes" class="menu-icon" src="./assets/icons/3-ReinaReyes.png" alt="Reina Reyes"></img></a></div>
-        <div class="icon"><a href="women.php?w=karmina-constantino"><img id="img-constantino" class="menu-icon down" src="./assets/icons/4-KarminaConstantino.png" alt="Karmina Constantino"></img></a></div>
-        <div class="icon"><a href="women.php?w=hidilyn-diaz"><img id="img-diaz" class="menu-icon up sports" src="./assets/icons/5-HidilynDiaz.png" alt="Hidilyn Diaz"></img></a></div>
-        <div class="icon"><a href="women.php?w=serena-williams"><img id="img-williams" class="menu-icon sports" src="./assets/icons/6-SerenaWilliams.png" alt="Serena Williams"></img></a></div>
-        <div class="icon"><a href="women.php?w=dolly-deleon"><img id="img-deleon" class="menu-icon" src="./assets/icons/7-DollyDeLeon.png" alt="Dolly de Leon"></img></a></div>
-        <div class="icon"><a href="women.php?w=edith-tiempo"><img id="img-tiempo" class="menu-icon up" src="./assets/icons/8-EdithTiempo.png" alt="Edith Tiempo"></img></a></div>
+        <div class="icon"><a href="women.php?w=maria-ressa" title="Maria Ressa"><img id="img-ressa" class="menu-icon" src="./assets/icons/1-MariaRessa.png" alt="Maria Ressa"></img></a></div>
+        <div class="icon"><a href="women.php?w=chona-abeledo" title="Chona Abeledo"><img id="img-abeledo" class="menu-icon down" src="./assets/icons/2-ChonaAbeledo.png" alt="Chona Abeledo"></img></a></div>
+        <div class="icon"><a href="women.php?w=reina-reyes" title="Reinabelle Reyes"><img id="img-reyes" class="menu-icon" src="./assets/icons/3-ReinaReyes.png" alt="Reina Reyes"></img></a></div>
+        <div class="icon"><a href="women.php?w=karmina-constantino" title="Karmina Constantino"><img id="img-constantino" class="menu-icon down" src="./assets/icons/4-KarminaConstantino.png" alt="Karmina Constantino"></img></a></div>
+        <div class="icon"><a href="women.php?w=hidilyn-diaz" title="Hidilyn Diaz"><img id="img-diaz" class="menu-icon up sports" src="./assets/icons/5-HidilynDiaz.png" alt="Hidilyn Diaz"></img></a></div>
+        <div class="icon"><a href="women.php?w=serena-williams" title="Serena Williams"><img id="img-williams" class="menu-icon sports" src="./assets/icons/6-SerenaWilliams.png" alt="Serena Williams"></img></a></div>
+        <div class="icon"><a href="women.php?w=dolly-deleon" title="Dolly de Leon"><img id="img-deleon" class="menu-icon" src="./assets/icons/7-DollyDeLeon.png" alt="Dolly de Leon"></img></a></div>
+        <div class="icon"><a href="women.php?w=edith-tiempo" title="Edith Tiempo"><img id="img-tiempo" class="menu-icon up" src="./assets/icons/8-EdithTiempo.png" alt="Edith Tiempo"></img></a></div>
         </div>
         
         <button class="nav-btn" id="nav-next"><i class="bi bi-chevron-right"></i></button>
     </div>
 </div>
-<!--<div id="menu-instructions"><p>Scroll through the icons and click to learn more!</p></div>-->
