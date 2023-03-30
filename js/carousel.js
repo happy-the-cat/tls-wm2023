1af841932a45e8c56b730324ea02b5e71a45732a
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-30 11:20:46 +0800

    Keep original icon carousel JS in comments (backup)

--- js/carousel.js
+++ js/carousel.js
@@ -48,3 +48,25 @@ navNext.addEventListener('click', () => {
     });
   }
 });
+
+/*
+
+const navPrev = document.querySelector('#nav-prev');
+const navNext = document.querySelector('#nav-next');
+const slider = document.querySelector('#menu-icons');
+
+navPrev.addEventListener('click', () => {
+  slider.scrollBy({
+    left: -200,
+    behavior: 'smooth'
+  });
+});
+
+navNext.addEventListener('click', () => {
+  slider.scrollBy({
+    left: 200,
+    behavior: 'smooth'
+  });
+});
+
+*/
