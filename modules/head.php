5e8f56e943350e4efe3612b84e1e511156e5d101
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-30 21:13:23 +0800

    Favicons

--- modules/head.php
+++ modules/head.php
@@ -3,6 +3,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Women's Month 2023 | The LaSallian</title>
 
+    <!-- Favicons -->
+    <link rel="icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" sizes="32x32" />
+    <link rel="icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" />
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
