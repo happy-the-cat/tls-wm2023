16a0e804752d34f9e1b604eeca9af2cffc226020
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-31 10:28:46 +0800

    Add poptown credit + shrink credit font

--- modules/footer.php
+++ modules/footer.php
@@ -6,6 +6,12 @@
     <div class="text">
         <p class="about"><b>The LaSallian</b> is the official student publication of De La Salle University. It is of the students, by the students, and for the students. Our student writers, photographers, videographers, artists, and web managers are committed to the 62-year tradition of journalistic excellence and issue-oriented critical thinking.</p>
         <p class="redirect"><a href="https://thelasallian.com/">Go to Main Website</a></p>
-        <p class="credit">Header Art by Addie Holgado<br>Avatars by Pat Soriano<br>Website by <a href="https://github.com/elliamaeang" target="_blank">Elliamae Ang</a> and <a href="https://github.com/happy-the-cat" target="_blank">Joyce Cai</a>        </p>
+        <p class="credit">
+            Header Art by Addie Holgado<br>
+            Avatars by Pat Soriano<br>
+            Poptown Illustrations by Gwen Roco<br>
+            Poptown Story by Marie Angeli Peña and Red Binay<br>
+            Website by <a href="https://github.com/elliamaeang" target="_blank">Elliamae Ang</a> and <a href="https://github.com/happy-the-cat" target="_blank">Joyce Cai</a>
+        </p>
     </div>
 </div>
